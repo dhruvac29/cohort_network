3662d8c16effff27b58363470b59728435fca3a6
dhruvac29 <dhruvac29@gmail.com>
2024-08-05 23:53:15 -0500

    Feat: analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,19 @@ import ContactUsPage from "./ContactUsPage";
 import CohortNetworkPage from "./CohortNetworkPage";
 import "./App.css";
 import Header from "./Header";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
     <Router>
       <Header />
+      <Analytics />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/contact-us" element={<ContactUsPage />} />
         <Route path="/cohort-network" element={<CohortNetworkPage />} />
         <Route path="/about" element={<HomePage />} />
       </Routes>
-      
     </Router>
   );
 };
